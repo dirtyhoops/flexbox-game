5165a91d759142c5849d85b7e0e620fd1d0484ff
Daryll <dosis@csumb.edu>
2023-03-11 11:35:57 -0800

    changed rocksCount to be an array instead of it changing flex alignment

--- src/components/Rocks.js
+++ src/components/Rocks.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-const Rocks = ({ rocksCount, rocksAllignment }) => {
+const Rocks = ({ rocksCount, rocksAllignment, rocksPlacement }) => {
   return (
+    // <div className={`rocks ${rocksAllignment}`}>
+    //   {/* <div className='rocks'> */}
+    //   {Array.from({ length: rocksCount }, (_, i) => (
+    //     <div key={i} className='rock'>
+    //       {i}
+    //       <p>remove this later</p>
+    //     </div>
+    //   ))}
+
+    //   {/* <div className='rock'></div> */}
+    // </div>
     <div className={`rocks ${rocksAllignment}`}>
-      {/* <div className='rocks'> */}
-      {Array.from({ length: rocksCount }, (_, i) => (
-        <div key={i} className='rock'>
-          {i}
-          <p>remove this later</p>
+      {rocksPlacement.map((rock, idx) => (
+        <div
+          key={idx}
+          className={`rock ${rock === 1 ? 'rock-show' : 'rock-hide'} `}
+        >
+          <p>{rock}</p>
         </div>
       ))}
-
-      {/* <div className='rock'></div> */}
     </div>
   );
 };
